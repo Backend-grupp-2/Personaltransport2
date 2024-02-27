286a709b1bd7fc028777e595fb4685662e953e84
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-02-27 14:25:19 +0100

    first commmit

---
+++ src/main/java/com/example/personaltransport/PersonalTransportApplication.java
@@ -0,0 +1,20 @@
+package com.example.personaltransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class PersonalTransportApplication {
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTransportApplication.class, args);
+	}
+
+}
