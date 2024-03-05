c38bdba6fe8b6002ad8772f37955344043354243
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-03-05 20:58:52 +0100

    new commit

--- src/main/java/com/example/personaltransport/controller/TransportController.java
+++ src/main/java/com/example/personaltransport/controller/TransportController.java
@@ -1,15 +1,15 @@
 package com.example.personaltransport.controller;
 
-import com.example.personaltransport.model.Route;
-import com.example.personaltransport.model.RouteObj;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+        import com.example.personaltransport.model.Route;
+        import com.example.personaltransport.model.RouteObj;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+        import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/personaltransport2")
