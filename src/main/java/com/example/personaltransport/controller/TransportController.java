bdc3177ab711596d4e27cc09e73ee69ae2c2776b
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-03-05 17:50:09 +0100

    changed url to find API

--- src/main/java/com/example/personaltransport/controller/TransportController.java
+++ src/main/java/com/example/personaltransport/controller/TransportController.java
@@ -20,28 +20,28 @@ public class TransportController {
 
     @GetMapping("/route/{start}/{end}/{transportType}")
     public List<Route> getBusRoutes(@PathVariable String start, @PathVariable String end, @PathVariable String transportType) {
-        String url = "http://localhost:8081/api/v1/transportroutes/route/" + start + "/" + end + "/" + transportType;
+        String url = "https://routesgithub.azurewebsites.net/route/" + start + "/" + end + "/" + transportType;
         RouteObj routeList = restTemplate.getForObject(url, RouteObj.class);
         return routeList.getRoutes();
     }
 
     @GetMapping("/route/{transportType}")
     public List<Route> getCarRoutes(@PathVariable String transportType){
-        String url = "http://localhost:8081/api/v1/transportroutesgithub/route/" + transportType;
+        String url = "https://routesgithub.azurewebsites.net/route/" + transportType;
         RouteObj routeList = restTemplate.getForObject(url, RouteObj.class);
         return routeList.getRoutes();
     }
 
     @GetMapping("/route/end/{end}")
     public List<Route> getBusRoutesFromStation(@PathVariable String end) {
-        String url = "http://localhost:8081/api/v1/transportroutesgithub/route/end/" + end;
+        String url = "https://routesgithub.azurewebsites.net/route/end/" + end;
         RouteObj routeList = restTemplate.getForObject(url, RouteObj.class);
         return routeList.getRoutes();
     }
 
     @GetMapping("/route/save/{id}")
     public Route saveRoute(@PathVariable Long id) {
-        String url = "http://localhost:8081/api/v1/transportroutesgithub/route/save/" + id;
+        String url = "https://routesgithub.azurewebsites.net/route/save/" + id;
         return restTemplate.getForObject(url, Route.class);
     }
 }
